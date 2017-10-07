a1b288f9b8483ade0be405bb0da521b4f0125426
Tran Dinh Nhan <trandinhnhanbk@MacBook-Air-cua-Tran.local>
2017-10-07 19:22:31 +0700

    Update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
   	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
   	<link href="css/font-awesome.min.css" rel="stylesheet">
   	<link href="css/bootstrap.min.css" rel="stylesheet">
-  	<link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet">  
+  	<link href="css/bootstrap-datetimepicker.min.css" rel="stylesheet">
   	<link href="css/flexslider.css" rel="stylesheet">
   	<link href="css/templatemo-style.css" rel="stylesheet">
   	<link href="css/login.css" rel="stylesheet">
@@ -14,21 +14,25 @@
   	<link href="css/bookingform/main.css" rel="stylesheet">
   	<link href="css/booking.css" rel="stylesheet">
 	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
-  	<script type="text/javascript" src="js/moment.js"></script>							
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>					
+  	<script type="text/javascript" src="js/moment.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap-datetimepicker.min.js"></script>
 	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
    	<script type="text/javascript" src="js/templatemo-script.js"></script>
    	<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+		<link  href = " //cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css "  rel = " stylesheet " >
+		<script  src = " //cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js " > </script>
+		<script  src = " //cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js " > </script>
+		{!! Toastr :: render () !!}
 
 </head>
 <body class="tm-gray-bg">
-	
+
   	<div class="tm-header">
   		<div class="container">
   			<div class="row">
   				<div class="col-lg-6 col-md-4 col-sm-3 tm-site-name-container">
-  					<a href="{{url('/')}}" class="tm-site-name">Hotel</a>	
+  					<a href="{{url('/')}}" class="tm-site-name">Hotel</a>
   				</div>
 	  			<div class="col-lg-6 col-md-8 col-sm-9">
 	  				<div class="mobile-menu-icon">
@@ -41,10 +45,10 @@
 							<li><a href="{{ 'booking' }}">Booking</a></li>
 							<li><a href="{{ 'login' }}">Login</a></li>
 						</ul>
-					</nav>		
-	  			</div>				
+					</nav>
+	  			</div>
   			</div>
-  		</div>	  	
+  		</div>
   	</div>
 
 	@yield('content')
@@ -52,12 +56,12 @@
 	<footer class="tm-black-bg">
 		<div class="container">
 			<div class="row">
-				<p class="tm-copyright-text">Copyright &copy; 2084 Your Company Name 
-                
+				<p class="tm-copyright-text">Copyright &copy; 2084 Your Company Name
+
                 | Designed by <a rel="nofollow" href="http://www.templatemo.com" target="_parent">templatemo</a></p>
 			</div>
-		</div>		
+		</div>
 	</footer>
-	
+
 </body>
 </html>

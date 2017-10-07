cbed17de54e1e4b1ee3e04e6a9867d5c7356dc46
nhatminh <meod.0003@gmail.com>
2017-10-07 08:42:10 +0700

    create room_types_seeder

---
+++ database/seeds/room_types_seeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class room_types_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('room_types')->insert([
+        	['id' =>1 , 'name' => 'VIP room'],
+        	['id' =>2 , 'name' => 'Deluxe room'],
+        	['id' =>3 , 'name' => 'Family room'],
+        ]);
+    }
+}

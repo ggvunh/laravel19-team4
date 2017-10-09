584db6367c05e831926b1e06b0046c5b7fb57119
nhatminh <meod.0003@gmail.com>
2017-10-09 12:13:27 +0700

    edit layout (date:10/09/17)

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('menu')
+	<a href="">dsadsadasdas</a>
+@stop
+
+@section('content')
+	<h3>
+		<a href="">Search Room</a><br>
+		<a href="">Room CRUD</a><br>
+		<a href="">Promotion CRUD</a>
+	</h3>
+@stop

94798fb18cdd68005283f64ed01aefdbe8fafe28
nhatminh <meod.0003@gmail.com>
2017-10-19 04:00:00 +0700

    edit model, view, fix error

--- app/Booking.php
+++ app/Booking.php
@@ -9,13 +9,14 @@ class Booking extends Model
 	protected $table = 'bookings';
 	protected $fillable = ['id', 'booking_code','check_in_date','check_out_date','status','cancelled_date','user_id'];
 
+    public function rooms()
+    {
+        return $this->belongsToMany('App\Room');
+    }
+    
     public function user()
     {
     	return $this->belongsTo('App\User', 'user_id');
     }
-
-        public function bookRooms()
-    {
-        return $this->hasMany('App\Book_Room');
-    }
+    
 }
--- resources/views/hotels/promotions/edit.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.admin')
-@section('content')
- 	<h1>Edit Promotion</h1><br>
-  	{!! Form::model($promotion, ['url' => 'admin/promotion/' . $promotion->id, 'method' => 'put']) !!}
-    @include('hotels.promotions.forms.promotion')
-  {!! Form::close() !!}
-@stop
